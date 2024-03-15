40ff7e830afed3fe7903c987621e70f0c2d4635b
francesamaniego <francemsaniego8@gmail.com>
2024-03-15 14:43:00 +0800

    animation update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,25 +11,25 @@ const Footer = () => {
   }, [])
   return (
     <footer className="footer" id="footer">
-      <div className="footer-holder">
-        <h1 data-aos="fade-right">Get in Touch</h1>
-        <h3 data-aos="fade-left">I’m currently looking for any new opportunities, my inbox is always open.</h3>
+      <div data-aos="fade-right" className="footer-holder">
+        <h1 >Get in Touch</h1>
+        <h3 >I’m currently looking for any new opportunities, my inbox is always open.</h3>
         <form action="mailto:francesamaniego21@gmail.com">
-            <button data-aos="fade-right" className="btn btn-s" >Send Mail</button>
+            <button  className="btn btn-s" >Send Mail</button>
         </form>
         <div  className="socmed-holder">
-          <a data-aos="fade-up" href='https://www.facebook.com/samaniego28/'> 
+          <a href='https://www.facebook.com/samaniego28/'> 
             <button className="socmed" type='submit'>
               <img src={fb} alt="fb" />
             </button>
           </a>
-          <a data-aos="fade-down" href='https://www.linkedin.com/in/france-samaniego-0a2011227/'> 
+          <a  href='https://www.linkedin.com/in/france-samaniego-0a2011227/'> 
             <button className="socmed" type='submit'>
               <img src={linkedin} alt="linkedin" />
             </button>
           </a>
         </div>
-        <h3 data-aos="fade-up">Designed and Built by France Samaniego</h3>
+        <h3>Designed and Built by France Samaniego</h3>
       </div>
     </footer>
   )
